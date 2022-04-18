3977ce7998fcda10ecf07b7070af7385e0733eab
Eyder Sepulveda <eydersepulveda@hotmail.com>
2022-04-18 21:18:06 +0200

    Correcciones Main Activity

--- app/src/main/java/com/eyder/worldwide/vistas/MainActivity.java
+++ app/src/main/java/com/eyder/worldwide/vistas/MainActivity.java
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.eyder.worldwide.R;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnIniciar = findViewById(R.id.btnIniciarSesion);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        btnIniciar.setOnClickListener(view -> iniciar());
     }
 
-    public void iniciar(View view) {
+    public void iniciar() {
         Intent i = new Intent(this, IniciarSesionRegistroActivity.class);
         startActivity(i);
     }
