48c443a6d257047e0165a3b70113ae752e2b98bf
Eyder Sepulveda <eydersepulveda@hotmail.com>
2022-05-21 21:06:58 +0200

    Implementación de  Google AdMob para publicidad

--- app/src/main/java/com/eyder/worldwide/controlador/HomeActivity.java
+++ app/src/main/java/com/eyder/worldwide/controlador/HomeActivity.java
@@ -3,12 +3,23 @@ package com.eyder.worldwide.controlador;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import com.eyder.worldwide.R;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -26,6 +37,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private GoogleSignInAccount gAccount;
     private BottomNavigationView bottomNavigationView;
     private CardView card1, card2, card3, card4;
+    private InterstitialAd mInterstitialAd;
 
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -67,6 +79,61 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 .requestEmail()
                 .build();
         gsc = GoogleSignIn.getClient(this, gso);
+
+        // Publicidad
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {}
+        });
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+                        Log.i("ANUNCIO", "CARGADO");
+                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+                                // Called when fullscreen content is dismissed.
+                                Log.d("TAG", "HA PASADO ALGO BUENO");
+                            }
+
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                // Called when fullscreen content failed to show.
+                                Log.d("TAG", "NO SE MOSTROS");
+                            }
+
+                            @Override
+                            public void onAdShowedFullScreenContent() {
+                                // Called when fullscreen content is shown.
+                                // Make sure to set your reference to null so you don't
+                                // show it a second time.
+                                mInterstitialAd = null;
+                                Log.d("TAG", "SE MOSTRO ALGP");
+                            }
+                        });
+
+                        if (mInterstitialAd != null) {
+                            mInterstitialAd.show(HomeActivity.this);
+                        } else {
+                            Log.d("TAG", "FALLO AL MOSTRARSE");
+                        }
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i("ANUNCIO", loadAdError.getMessage());
+                        mInterstitialAd = null;
+                    }
+                });
+
 
     }
 
